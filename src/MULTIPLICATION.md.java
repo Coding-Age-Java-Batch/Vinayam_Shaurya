e52d0a9b39b93bf8184185f954d98dcb8c1ba041
35262021223526 <119745346+35262021223526@users.noreply.github.com>
2026-02-12 17:26:14 +0530

    Delete src/MULTIPLICATION.md.java

--- src/MULTIPLICATION.md.java
+++
@@ -1,9 +0,0 @@
-class Multiplication {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 4;
-        int product = a * b;
-
-        System.out.println("Multiplication = " + product);
-    }
-}
